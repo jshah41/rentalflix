4a91e9e08fbbe0b12c8b4f4e82995d569cb1105d
Jenish Shah <Jenish Shah>
2016-02-13 21:11:36 -0600

    <Rentalflix Assesment>: Testcases File

--- src/test/java/io/egen/rentalflix/MovieServiceTest.java
+++ src/test/java/io/egen/rentalflix/MovieServiceTest.java
@@ -1,8 +1,45 @@
 package io.egen.rentalflix;
 
+import junit.framework.Test;
+import org.junit.*;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+import java.util.*;
+
 /**
- * JUnit test cases for MovieService
+ * Unit test for simple App.
  */
-public class MovieServiceTest {
+public class AppTest 
+    extends TestCase
+    {
+List<Movie> testList=new ArrayList<Movie>();
+	
+	
+	
+	
+	MovieService m=new MovieService();
+	
+	
+	int testRent=1;
+	
+	
+	public void test1(){
+		boolean b=m.rentMovie(1, "jenish");
+		assertEquals(true, b);	
+		
+	}
+	
+	
+	
+	public void test2(){
+		m.create(new Movie(1, "ABCD", 2017, "English" ));
+		testList.add(new Movie(1, "ABCD", 2017, "English" ));
+		
+		List<Movie> ans=m.findAll();
+		
+		assertEquals(testList.size(), ans.size());
+		
+		
+	}
 
 }
